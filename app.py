7fb358dbdbaf6d641ac052dab8759e1d6a387fe5
Fernando Borretti <eudoxiahp@gmail.com>
2017-02-19 18:25:11 -0300

    Working table creation, scraper

--- app.py
+++ app.py
@@ -16,32 +16,49 @@ threads_table = sa.Table('threads', metadata,
                          sa.Column('url', sa.Text()))
 
 
-async def ensure_tables_exist(engine):
-    async with engine.acquire() as conn:
-        await conn.execute('DROP TABLE IF EXISTS ')
-        await conn.execute('''CREATE TABLE tbl (
-                                  id serial PRIMARY KEY,
-                                  val varchar(255))''')
-
-
-async def go():
+async def ensure_tables_exist():
     async with create_engine(user='postgres',
                              password='postgres',
                              database='postgres',
                              host='postgres') as engine:
-        await ensure_tables_exist(engine)
+        async with engine.acquire() as conn:
+            await conn.execute('DROP TABLE threads')
+            await conn.execute(sa.schema.CreateTable(threads_table))
         print("Created tables!")
 
 # Scraper
 
 from aiohttp import client
+from bs4 import BeautifulSoup
 
-async def get_front_page_html():
+
+async def get_front_page():
     response = await client.get('https://news.ycombinator.com/')
-    return (await response.read())
+    html = await response.read()
+    soup = BeautifulSoup(html, 'html.parser')
+    links = soup.find_all('a')
+
+    # Python 3.6 lets supports asynchronous generators, which would fit this
+    # case, but for this example returning the full list is fine since all the
+    # processing is in the CPU
+
+    def parse_link(link):
+        if link.get('class', None) == ['storylink']:
+            url = link['href']
+            text = link.string
+            # Despite the aiohttp documentation, the response is not UTF-8 encoded
+            return (url, text.encode('utf-8'))
+
+    return filter(lambda x: x is not None, [parse_link(link) for link in links])
 
 # Entry point
 
 loop = asyncio.get_event_loop()
-print(loop.run_until_complete(get_front_page_html()))
+loop.run_until_complete(ensure_tables_exist())
+links = loop.run_until_complete(get_front_page())
+for l in links:
+    try:
+        print(l)
+    except:
+        print("Error")
 loop.close()
