c89aabbc0185cd8fd8f8c4c1c329c74136a02f1f
Fernando Borretti <eudoxiahp@gmail.com>
2017-02-19 16:56:20 -0300

    Add db code

--- app.py
+++ app.py
@@ -1,5 +1,33 @@
 import asyncio
+
+print("Hello, world!")
+
+# Database
+
 import sqlalchemy as sa
 from aiopg.sa import create_engine
 
-print("Hello, world!")
+
+metadata = sa.MetaData()
+
+threads_table = sa.Table('threads', metadata,
+                         sa.Column('id', sa.Integer, primary_key=True),
+                         sa.Column('title', sa.Text()),
+                         sa.Column('url', sa.Text()))
+
+
+async def ensure_tables_exist(engine):
+    async with engine.acquire() as conn:
+        await conn.execute('DROP TABLE IF EXISTS ')
+        await conn.execute('''CREATE TABLE tbl (
+                                  id serial PRIMARY KEY,
+                                  val varchar(255))''')
+
+
+async def go():
+    async with create_engine(user='pg',
+                             database='hn',
+                             host='postgres',
+                             password='pg') as engine:
+        await ensure_tables_exist(engine)
+        print("Created tables!")
